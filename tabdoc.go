742daaf067535dd42a028f4098bd72908a3d56bf
mbuto <mbuto11@gmail.com>
2019-11-01 14:26:44 +0100

    lallero

--- tabdoc.go
+++ tabdoc.go
@@ -1,1 +1,46 @@
-/home/a/APP/libuni-oggi/tabdoc.go
+package main
+
+const docpref = "p/docente-"
+
+type tbdoc struct {
+link string
+}
+
+var doctab = []tbdoc {
+tbdoc { "aldo-sorci" },
+tbdoc { "don-simone--mauro-pesce-a.rita-caponera" },
+tbdoc { "perli" },
+tbdoc { "sabrina-batino" },
+tbdoc { "meo" },
+tbdoc { "massimo-neri" },
+tbdoc { "laura-fatini" },
+tbdoc { "lucia-paoletti" },
+tbdoc { "colaiacomo-festuccia-margaritelli" },
+tbdoc { "aliotta" },
+tbdoc { "pittaro" },
+tbdoc { "andrea-colaiacomo" },
+tbdoc { "marilena-rossi" },
+tbdoc { "caselli" },
+tbdoc { "bordoni" },
+tbdoc { "carlo-zappala" },
+tbdoc { "batino" },
+tbdoc { "mastella" },
+tbdoc { "ganz" },
+tbdoc { "carlo-zappala" },
+tbdoc { "faletra" },
+tbdoc { "luca-marchegiani" },
+tbdoc { "franco-boschi" },
+tbdoc { "lorenzi" },
+tbdoc { "botti-lorenzi" },
+tbdoc { "botti" },
+tbdoc { "pandolfo" },
+tbdoc { "pandolfo" },
+tbdoc { "pandolfo" },
+tbdoc { "pandolfo" },
+tbdoc { "pandolfo" },
+tbdoc { "pandolfo" },
+tbdoc { "pandolfo" },
+tbdoc { "pandolfo" },
+tbdoc { "evaristo-seghetta" },
+tbdoc { "angela-cingotti" },
+}
--- tablink.go
+++ tablink.go
@@ -1,1 +1,47 @@
-/home/a/APP/libuni-oggi/tablink.go
+package main
+
+const linkpref = "2019/08/"
+
+type tblink struct {
+numco int
+link string
+}
+
+var linktab = []tblink {
+tblink { 130,"economia.html" },
+tblink { 131,"le-tre-grandi-religioni.html" },
+tblink { 132,"arte-contemporanea.html" },
+tblink { 133,"etruscologia-1.html" },
+tblink { 134,"letteratura-inglese.html" },
+tblink { 135,"storia-pievese.html" },
+tblink { 136,"teatro.html" },
+tblink { 137,"italiano.html" },
+tblink { 138,"opera-lirica.html" },
+tblink { 139,"musica-jazz.html" },
+tblink { 140,"storia-contemporanea.html" },
+tblink { 141,"storia-del-cinema.html" },
+tblink { 142,"personaggi-dellumbria.html" },
+tblink { 143,"astronomia.html" },
+tblink { 144,"cultura-classica.html" },
+tblink { 145,"greco-antico.html" },
+tblink { 146,"etruscologia-2.html" },
+tblink { 147,"letteratura-sn.html" },
+tblink { 148,"naturopatia.html" },
+tblink { 149,"intelligenza-artificiale.html" },
+tblink { 150,"poesia.html" },
+tblink { 151,"storia-dellarte.html" },
+tblink { 152,"storia-del-territorio.html" },
+tblink { 153,"danza-in-cerchio-2.html" },
+tblink { 154,"danza-in-cerchio-1.html" },
+tblink { 155,"danza-in-cerchio-3.html" },
+tblink { 156,"yoga-2-mer.html" },
+tblink { 157,"yoga-1-lun.html" },
+tblink { 158,"yoga-4-mer.html" },
+tblink { 159,"yoga-1-mer.html" },
+tblink { 160,"yoga-3-mer.html" },
+tblink { 161,"yoga-3-lun.html" },
+tblink { 162,"yoga-4-lun.html" },
+tblink { 163,"yoga-2-lun.html" },
+tblink { 901,"i-soci-raccontano.html" },
+tblink { 902,"presentaz.-libro-semboloni.html" },
+}
