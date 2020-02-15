255122a6acc5e802ef87d5832b8e18b526ec0599
mbuto <mbuto11@gmail.com>
2020-02-15 18:33:09 +0100

    tabelle variabili

--- anacor.go
+++ anacor.go
@@ -42,5 +42,6 @@ anacor { 161,"Yoga 3 Lun","Pandolfo","19.00-20.30","3°Piano","" },
 anacor { 162,"Yoga 4 Lun","Pandolfo","19.00-20.30","3°Piano","" },
 anacor { 163,"Yoga 2 Lun","Pandolfo","19.00-20.30","3°Piano","" },
 anacor { 907,"I Soci Raccontano","Carlo Giuliani","17.00-18.30","","" },
+anacor { 908,"Pres. Libro","S. Batino","17.00-18.30","","" },
 }
-const verac="200213.1420"
+const verac="200215.0939"
--- tabdoc.go
+++ tabdoc.go
@@ -39,4 +39,5 @@ tbdoc { "pandolfo" },
 tbdoc { "pandolfo" },
 tbdoc { "pandolfo" },
 tbdoc { "carlo-giuliani" },
+tbdoc { "s.-batino" },
 }
