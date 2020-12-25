77b7b281b819af353dd0e7a0c4b969fe55f2787c
mbuto <mbuto11@gmail.com>
2020-12-25 20:50:08 +0100

    tabelle variabili

---
+++ splash.go
@@ -0,0 +1,13 @@
+package main
+
+const formsplash = `
+<!-- empty ? -->
+<div class=w3-container>
+<div class='w3-panel w3-yellow'>
+<b>ATTENZIONE: TUTTE LE ATTIVIT&Agrave; SONO SOSPESE FINO AL 15 GENNAIO 2021<br>
+IN OTTEMPERANZA ALLA ORDINANZA REG. UMBRIA N. 77 DEL 4 DICEMBRE 2020
+</b><br>
+</div>
+</div>
+<!-- not empty -->
+`
