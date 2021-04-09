ce4b95d3251d9f68026a1ddeb29adfdec3a51794
mbuto <mbuto11@gmail.com>
2021-04-09 09:18:16 +0200

    tabelle variabili

--- anacor.go
+++ anacor.go
@@ -11,8 +11,8 @@ tutor string
 }
 
 var corsi = []anacor {
-anacor { 180, "Archeologia", "Sabrina Batino", "16.00-17.00", "Online", "C.Zappalà" },
-anacor { 181, "Le donne di Dante", "Lucia Paoletti", "16.00-17.00", "Online", "C.Zappalà" },
-anacor { 1000, "Social Network", "Carlo Zappalà", "16.00-17.00", "Online", "C.Zappalà" },
+anacor { 180, "Archeologia", "Sabrina Batino", "15.30-17.00", "", "" },
+anacor { 181, "Le donne di Dante", "Lucia Paoletti", "15.30-17.00", "", "" },
+anacor { 1000, "Social Network", "Carlo Zappalà", "15.30-17.00", "", "" },
 }
-const verac="2021-03-21 15:15:15"
+const verac="2021-04-09 09:16:26"
