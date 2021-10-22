b87361e99e302891c425c20d484f8b8c4ebf4c95
mbuto <mbuto11@gmail.com>
2021-10-22 12:56:55 +0200

    tabelle variabili

--- anacor.go
+++ anacor.go
@@ -12,7 +12,7 @@ tutor string
 
 var corsi = []anacor {
 anacor { 180, "Leonardo da Vinci e Pietro Vannucci: due protagonisti a confronto", "Luca Marchegiani", "15.30-17.00", "Sala Grande", "" },
-anacor { 181, "Un viaggio negli archivi umbri, fra Rinascimento e Novecento, sulle tracce di personaggi noti e meno noti", "Marilena Rossi Caponeri", "15.30-17.00", "Sala Grande*", "La lezione del 28 ott. si svolge a PG" },
+anacor { 181, "Un viaggio negli archivi umbri, fra Rinascimento e Novecento, sulle tracce di personaggi noti e meno noti", "Marilena Rossi Caponeri", "15.30-17.00", "Sala Grande*", "La lezione del 28 ott. si svolge ad Orvieto presso la sezione di Archivio di Stato" },
 anacor { 182, "Le donne, reali e letterarie, di Dante", "Lucia Paoletti", "15.30-17.00", "Sala Grande", "" },
 anacor { 183, "Puccini e dintorni", "Andrea Colaiacomo, Giuliano Festuccia, Angela Margaritelli", "15.30-17.00", "Aula Libuni", "" },
 anacor { 184, "L_economia italiana prima e dopo il Recovery (Fondo nazionale di ripresa e resilienza)", "Aldo Sorci", "15.30-17.00", "Sala Grande", "" },
@@ -21,4 +21,4 @@ anacor { 186, "L_Ospedale di Città della Pieve nel suo secolo breve", "Massimo 
 anacor { 187, "Le donne nel Jazz: dalle origini all_era dello swing", "Claudia Aliotta", "15.30-17.00", "", "" },
 anacor { 1000, "Presentazione Corsi 2021", "Lucia Paoletti", "17.00-18:30", "", "" },
 }
-const verac="2021-10-22 11:50:19"
+const verac="2021-10-22 12:50:19"
