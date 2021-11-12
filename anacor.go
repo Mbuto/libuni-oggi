9eb876e393da8427bb1aec157985f09d6e00b442
mbuto <mbuto11@gmail.com>
2021-11-12 15:18:12 +0100

    tabelle variabili

--- anacor.go
+++ anacor.go
@@ -15,11 +15,10 @@ anacor { 180, "Leonardo da Vinci e Pietro Vannucci: due protagonisti a confronto
 anacor { 181, "Un viaggio negli archivi umbri, fra Rinascimento e Novecento, sulle tracce di personaggi noti e meno noti", "Marilena Rossi Caponeri", "15.30-17.00", "", "" },
 anacor { 182, "Le donne, reali e letterarie, di Dante", "Lucia Paoletti", "15.30-17.00", "Sala Grande", "" },
 anacor { 183, "Puccini e dintorni", "Andrea Colaiacomo, Giuliano Festuccia, Angela Margaritelli", "15.30-17.00", "", "" },
-anacor { 184, "L_economia italiana prima e dopo il Recovery (Fondo nazionale di ripresa e resilienza)", "Aldo Sorci", "15.30-17.00", "Sala delle Muse", "" },
+anacor { 184, "L'economia italiana prima e dopo il Recovery (Fondo nazionale di ripresa e resilienza)", "Aldo Sorci", "15.30-17.00", "Sala delle Muse", "" },
 anacor { 185, "Il mito di Edipo", "Silvia Bordoni", "15.30-17.00", "", "" },
-anacor { 186, "L_Ospedale di Città della Pieve nel suo secolo breve", "Massimo Neri", "15.30-17.00", "", "" },
+anacor { 186, "L'Ospedale di Città della Pieve nel suo secolo breve", "Massimo Neri", "15.30-17.00", "", "" },
 anacor { 187, "Le donne nel Jazz: dalle origini all_era dello swing", "Claudia Aliotta", "15.30-17.00", "", "" },
 anacor { 1000, "Visita Archivio di Stato - Orvieto", "Marilena Rossi Caponeri", "15.30-17.00", "Orvieto", "" },
-anacor { 1001, "Assemblea dei Soci per elezione delle Cariche Sociali", "", "15.30-17.00", "Sala delle Muse", "" },
 }
-const verac="2021-11-05 16:40:40"
+const verac="2021-10-22 16:42:40"
